9ce98113e6a7c177178e4fc331f301fc22c7b2c3
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2021-11-15 23:10:58 +0800

    首页基本画完了

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,67 +1,85 @@
 // index.js
 // const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    columns: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+    year:'2021',
+    // 年度选择
+    showYearPicker: false,
+    // 年度选择集合
+    yearData: ['2021', ],
   },
 
+  // 显示年度选择框
+  yearPickerShow() {
+    this.setData({
+      showYearPicker: true
+    })
+  },
+  // 年度选择框 改变
+  yearPickerChange(event) {
+    this.setData({
+      year: event.detail.value
+    });
+  },
+  // 关闭年度选择框
+  yearPickerClose() {
+    this.setData({
+      showYearPicker: false
+    });
+    // todo 加载对应年度的账单
+    // ...
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    this.getTabBar().init();
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -33,7 +33,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.getTabBar().init();
   },
 
   /**
