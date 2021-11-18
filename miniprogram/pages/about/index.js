6f9abeaf49e6639d504a942d8a32ae651529e258
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2021-11-18 21:57:58 +0800

    文档格式化

--- miniprogram/pages/about/index.js
+++ miniprogram/pages/about/index.js
@@ -7,7 +7,12 @@ Page({
     data: {
 
     },
-
+    goGuide(){
+        wx.showToast({
+            title: '开发中...',
+            icon:'none',
+          })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
