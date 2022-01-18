04ef9fee82d532e35454afbfe38d53de8ba12c0b
micahh28 <1067051515@qq.com>
2022-01-18 16:05:39 +0800

    优化 多处细节

--- miniprogram/pages/bookDetails/index.js
+++ miniprogram/pages/bookDetails/index.js
@@ -1,4 +1,5 @@
 const app = getApp()
+const dayjs = require('dayjs');
 const db = wx.cloud.database()
 Page({
     data: {
@@ -7,41 +8,56 @@ Page({
         pageNo: 0,
         pageEnd: false,
     },
-    // 分页获取数据
-    getPage(page, limit) {
-        return db.collection('gift')
-            .where({
-                userId: app.globalData.user._id,
-                bookId: this.data.bookId
+    formatDate(date) {
+        return dayjs(date).format('YYYY-MM-DD');
+    },
+    loadData(page) {
+        // 如果服务已经停止
+        if (app.globalData.serviceStopped) {
+            this.setData({
+                service_stopped: true,
+                giftList: []
             })
-            .orderBy('luckyDay', 'desc')
-            .skip(page * limit)
-            .limit(limit)
-            .get()
+            return
+        }
+        if (page == 0) {
+            this.data.giftList = []
+        }
+        let that = this
+        wx.cloud.callFunction({
+            name: 'lijiFunctions',
+            data: {
+                type: 'lookupGiftFriend',
+                page: page,
+                limit: 10,
+                where: {
+                    userId: app.globalData.user._id,
+                    bookId: this.data.bookId
+                }
+            }
+        }).then(res => {
+            if (res.result.list.length === 0) {
+                that.data.pageEnd = true
+                return
+            }
+            let datas = this.data.giftList.concat(res.result.list)
+            datas.map(i => {
+                if (i.luckDay) {
+                    i.luckDay = that.formatDate(i.luckDay)
+                }
+            })
+            that.setData({
+                giftList: datas,
+                pageNo: that.data.pageNo + 1
+            });
+        })
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        let that = this
-        that.setData({
-            bookId: options.bookId,
-        });
-        this.data.pageNo = 0
-        this.getPage(this.data.pageNo, 10).then(res => {
-            if (res.data.length === 0) {
-                this.setData({
-                    pageEnd: true,
-                });
-            }
-            this.setData({
-                giftList: res.data,
-                pageNo: this.data.pageNo + 1
-            });
-        })
-        wx.setNavigationBarTitle({
-            title: ''
-        })
+        this.data.bookId = options.bookId
+        this.loadData(0)
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -82,20 +98,8 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-        let that = this
         if (!this.data.pageEnd) {
-            // this.setData({
-            //   loading: true,
-            // });
-            this.getPage(this.data.pageNo, 10).then(res => {
-                if (res.data.length > 0) {
-                    let datas = that.data.giftList.concat(res.data)
-                    this.data.pageNo + 1
-                    this.setData({
-                        giftList: datas
-                    });
-                }
-            })
+            this.loadData(this.data.pageNo)
         }
     },
 
