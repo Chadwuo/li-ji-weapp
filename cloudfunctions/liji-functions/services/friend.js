74bc5fb11e481fe1b2c28b6aa47979ae48f1ae2d
micahh28 <41413559+micahh28@users.noreply.github.com>
2022-08-02 14:03:54 +0800

    Update friend.js

--- cloudfunctions/liji-functions/services/friend.js
+++ cloudfunctions/liji-functions/services/friend.js
@@ -6,6 +6,32 @@ cloud.init({
 
 const db = cloud.database();
 
+// 获取全部数据集合
+exports.getList = async (event, context) => {
+	const db = cloud.database()
+	const MAX_LIMIT = 100
+	exports.main = async (event, context) => {
+		// 先取出集合记录总数
+		const countResult = await db.collection('friend').count()
+		const total = countResult.total
+		// 计算需分几次取
+		const batchTimes = Math.ceil(total / 100)
+		// 承载所有读操作的 promise 的数组
+		const tasks = []
+		for (let i = 0; i < batchTimes; i++) {
+			const promise = db.collection('friend').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+			tasks.push(promise)
+		}
+		// 等待所有
+		return (await Promise.all(tasks)).reduce((acc, cur) => {
+			return {
+				data: acc.data.concat(cur.data),
+				errMsg: acc.errMsg,
+			}
+		})
+	}
+};
+
 // 添加
 exports.add = async (event, context) => {
 	const {
