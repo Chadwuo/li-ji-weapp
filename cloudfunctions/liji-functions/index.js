3d3f44e62a5c1d0d9044e7f527d9087e1772bf2c
micahh28 <41413559+micahh28@users.noreply.github.com>
2022-08-02 12:00:42 +0800

    Update index.js

--- cloudfunctions/liji-functions/index.js
+++ cloudfunctions/liji-functions/index.js
@@ -6,20 +6,32 @@ const lookupBookGift = require('./lookupBookGift/index');
 
 const userHandler = require('./services/user');
 const bookHandler = require('./services/book');
+const giftReceiveHandler = require('./services/giftReceive');
+const giftOutHandler = require('./services/giftOut');
 
 // 云函数入口函数
 exports.main = async (event, context) => {
     switch (event.type) {
-        case 'userOpenid':
+       case 'userOpenid': // 获取用户openid
             return await userHandler.userOpenid(event, context);
-        case 'getBookPage':
+        case 'getBookPage': // 分页获取礼簿
             return await bookHandler.page(event, context);
-        case 'addBook':
+        case 'addBook': //添加礼簿
             return await bookHandler.add(event, context);
-        case 'updateBook':
+        case 'updateBook': // 更新礼簿
             return await bookHandler.update(event, context);
-        case 'deleteBook':
+        case 'deleteBook': // 删除礼簿
             return await bookHandler.delete(event, context);
+        case 'addGiftReceive': // 添加收礼
+            return await giftReceiveHandler.add(event, context);
+        case 'updateGiftReceive': // 更新收礼
+            return await giftReceiveHandler.update(event, context);
+        case 'deleteGiftReceive': // 删除收礼
+            return await giftReceiveHandler.delete(event, context);
+        case 'addGiftReceive': // 添加送礼
+            return await giftOutHandler.add(event, context);
+        case 'updateGiftOut': // 更新送礼
+            return await giftOutHandler.upd
 
         case 'getUserInfo':
             return await getUserInfo.main(event, context);
