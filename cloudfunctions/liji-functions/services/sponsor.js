fac6f6b3116637685224e8fdbbf5899ef6b767e3
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-02 14:03:28 +0800

    Update sponsor.js

--- cloudfunctions/liji-functions/services/sponsor.js
+++ cloudfunctions/liji-functions/services/sponsor.js
@@ -19,7 +19,7 @@ exports.getList = async (event, context) => {
 		// 承载所有读操作的 promise 的数组
 		const tasks = []
 		for (let i = 0; i < batchTimes; i++) {
-			const promise = db.collection('todos').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+			const promise = db.collection('sponsor').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
 			tasks.push(promise)
 		}
 		// 等待所有
