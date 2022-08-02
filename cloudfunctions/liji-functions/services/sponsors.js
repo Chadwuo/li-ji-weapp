317f6c6b7dd62ccb2cf1a7a3c1dee943b2619691
micahh28 <41413559+micahh28@users.noreply.github.com>
2022-08-02 13:48:28 +0800

    Update sponsors.js

--- cloudfunctions/liji-functions/services/sponsors.js
+++ cloudfunctions/liji-functions/services/sponsors.js
@@ -6,6 +6,31 @@ cloud.init({
 
 const db = cloud.database();
 
+// 获取全部数据集合
+exports.getList = async (event, context) => {
+	const db = cloud.database()
+	const MAX_LIMIT = 100
+	exports.main = async (event, context) => {
+		// 先取出集合记录总数
+		const countResult = await db.collection('sponsors').count()
+		const total = countResult.total
+		// 计算需分几次取
+		const batchTimes = Math.ceil(total / 100)
+		// 承载所有读操作的 promise 的数组
+		const tasks = []
+		for (let i = 0; i < batchTimes; i++) {
+			const promise = db.collection('todos').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+			tasks.push(promise)
+		}
+		// 等待所有
+		return (await Promise.all(tasks)).reduce((acc, cur) => {
+			return {
+				data: acc.data.result.list.concat(cur.data.result.list),
+				errMsg: acc.errMsg,
+			}
+		})
+	}
+};
 
 // 添加
 exports.add = async (event, context) => {
@@ -13,10 +38,10 @@ exports.add = async (event, context) => {
 		data
 	} = event
 	try {
-		await db.collection('sponsors').add(data)
+		const res = await db.collection('sponsors').add(data)
 		return {
 			success: true,
-			data: ''
+			data: res
 		};
 	} catch (e) {
 		return {
