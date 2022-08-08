1c9437b8b4209d92f04ffcd8381916b45356ddf3
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-08 02:17:19 +0000

    new friend page

--- cloudfunctions/liji-functions/services/friend.js
+++ cloudfunctions/liji-functions/services/friend.js
@@ -1,5 +1,7 @@
 const cloud = require('wx-server-sdk');
-
+const {
+	getUserDataScope
+} = require('./user');
 cloud.init({
 	env: cloud.DYNAMIC_CURRENT_ENV
 });
@@ -8,27 +10,39 @@ const db = cloud.database();
 
 // 获取全部数据集合
 exports.getList = async (event, context) => {
-	const db = cloud.database()
-	const MAX_LIMIT = 100
-	exports.main = async (event, context) => {
-		// 先取出集合记录总数
-		const countResult = await db.collection('friend').count()
-		const total = countResult.total
-		// 计算需分几次取
-		const batchTimes = Math.ceil(total / 100)
-		// 承载所有读操作的 promise 的数组
-		const tasks = []
-		for (let i = 0; i < batchTimes; i++) {
-			const promise = db.collection('friend').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
-			tasks.push(promise)
-		}
-		// 等待所有
-		return (await Promise.all(tasks)).reduce((acc, cur) => {
-			return {
-				data: acc.data.concat(cur.data),
-				errMsg: acc.errMsg,
+	try {
+		// 数据权限范围
+		const dataScope = await getUserDataScope(event, context)
+		// 查询操作符
+		const _ = db.command
+		const MAX_LIMIT = 100
+		exports.main = async (event, context) => {
+			// 先取出集合记录总数
+			const countResult = await db.collection('friend').count()
+			const total = countResult.total
+			// 计算需分几次取
+			const batchTimes = Math.ceil(total / 100)
+			// 承载所有读操作的 promise 的数组
+			const tasks = []
+			for (let i = 0; i < batchTimes; i++) {
+				const promise = db.collection('friend').where({
+					userId: _.in(dataScope)
+				}).skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+				tasks.push(promise)
 			}
-		})
+			// 等待所有
+			return (await Promise.all(tasks)).reduce((acc, cur) => {
+				return {
+					data: acc.data.concat(cur.data),
+					message: acc.errMsg,
+				}
+			})
+		}
+	} catch (error) {
+		return {
+			success: false,
+			message: error,
+		}
 	}
 };
 
@@ -46,15 +60,18 @@ exports.add = async (event, context) => {
 	} catch (e) {
 		return {
 			success: false,
-			errMsg: e
+			message: e
 		};
 	}
 };
 
 // 更新
 exports.update = async (event, context) => {
+	const {
+		data
+	} = event
 	try {
-		await db.collection('friend').doc(event._id).update()
+		await db.collection('friend').doc(event._id).update(data)
 		return {
 			success: true,
 			data: ''
@@ -62,7 +79,7 @@ exports.update = async (event, context) => {
 	} catch (e) {
 		return {
 			success: false,
-			errMsg: e
+			message: e
 		};
 	}
 };
@@ -70,6 +87,15 @@ exports.update = async (event, context) => {
 // 删除
 exports.delete = async (event, context) => {
 	try {
+		// 删除亲友下所有送礼记录
+		await db.collection('giftOut').where({
+			friendId: event._id
+		}).remove()
+		// 删除亲友下所有收礼记录
+		await db.collection('giftReceive').where({
+			friendId: event._id
+		}).remove()
+		// 删除亲友
 		await db.collection('friend').doc(event._id).remove()
 		return {
 			success: true,
@@ -78,7 +104,7 @@ exports.delete = async (event, context) => {
 	} catch (e) {
 		return {
 			success: false,
-			errMsg: e
+			message: e
 		};
 	}
 };
