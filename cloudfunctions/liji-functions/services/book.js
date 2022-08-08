8f0d19e38b2563fb9052d4bfca1cfe640ebb6725
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-08 03:07:06 +0000

    new book select page

--- cloudfunctions/liji-functions/services/book.js
+++ cloudfunctions/liji-functions/services/book.js
@@ -40,6 +40,46 @@ exports.page = async (event, context) => {
   }
 };
 
+// 获取全部数据集合
+exports.getList = async (event, context) => {
+	try {
+		// 数据权限范围
+		const dataScope = await getUserDataScope(event, context)
+		// 查询操作符
+		const _ = db.command
+		const MAX_LIMIT = 100
+		exports.main = async (event, context) => {
+			// 先取出集合记录总数
+			const countResult = await db.collection('book').where({
+				userId: _.in(dataScope)
+			}).count()
+			const total = countResult.total
+			// 计算需分几次取
+			const batchTimes = Math.ceil(total / 100)
+			// 承载所有读操作的 promise 的数组
+			const tasks = []
+			for (let i = 0; i < batchTimes; i++) {
+				const promise = db.collection('book').where({
+					userId: _.in(dataScope)
+				}).skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+				tasks.push(promise)
+			}
+			// 等待所有
+			return (await Promise.all(tasks)).reduce((acc, cur) => {
+				return {
+					data: acc.data.concat(cur.data),
+					message: acc.errMsg,
+				}
+			})
+		}
+	} catch (error) {
+		return {
+			success: false,
+			message: error,
+		}
+	}
+};
+
 // 获取
 exports.get = async (event, context) => {
   try {
