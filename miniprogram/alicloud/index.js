12f4654c9bd8894c4883f07731215823778c7001
Chadwuo <chadwuo@outlook.com>
2022-08-10 22:57:25 +0800

    Work crazy

--- miniprogram/alicloud/index.js
+++ miniprogram/alicloud/index.js
@@ -1,12 +1,12 @@
 import MPServerless from '@alicloud/mpserverless-sdk'
 
 const mpserverless = new MPServerless(wx, {
-    appId: '小程序 AppID',
-    spaceId: '服务空间 SpaceId',
-    clientSecret: '服务空间 Secret',
-    endpoint: '服务空间 API Endpoint'
+  appId: '小程序 AppID',
+  spaceId: '服务空间 SpaceId',
+  clientSecret: '服务空间 Secret',
+  endpoint: '服务空间 API Endpoint'
 });
 
 module.exports = {
-    mpserverless
+  mpserverless
 }
--- miniprogram/alicloud/services/user.js
+++ miniprogram/alicloud/services/user.js
@@ -1,43 +1,52 @@
 const {
-    alicloud
+  mpserverless
 } = getApp();
+
 
 /**
  * 获取用户信息
  * 如果没有会自动创建用户
  *
- * @author micahh
+ * @author chadwuo
  */
 exports.getUserInfo = async () => {
-    try {
-        const res = await alicloud.user.getInfo()
-        if (res.success) {
-            const userId = res.result.userId // Serverless平台生成的用户ID
-            let user = await alicloud.db.collection('user').findOne({
-                _id: userId
-            })
-            if (!user) {
-                // 创建用户
-                user = {
-                    _id: userId,
-                    familyId: '',
-                    isVip: false
-                }
-                await alicloud.db.collection('user').insertOne(user)
-            }
-            return {
-                success: true,
-                data: user
-            }
+  try {
+    const res = await mpserverless.user.getInfo()
+    if (res.success) {
+      const userId = res.result.user.userId // Serverless平台生成的用户ID
+      let {
+        result: user
+      } = await mpserverless.db.collection('user').findOne({
+        _id: userId
+      })
+
+      console.log('3333')
+      if (!user) {
+        // 创建用户
+        user = {
+          _id: userId,
+          familyId: '',
+          isVip: false
         }
-        return {
-            success: false,
-            message: '操作失败'
-        }
-    } catch (e) {
-        return {
-            success: false,
-            message: e
-        };
+        mpserverless.db.collection('user').insertOne(user).then(res => {
+          console.log(res)
+        }).catch(console.error);
+      }
+
+      console.log('222222')
+      return {
+        success: true,
+        data: user
+      }
     }
+    return {
+      success: false,
+      message: '操作失败'
+    }
+  } catch (e) {
+    return {
+      success: false,
+      message: e
+    };
+  }
 }
