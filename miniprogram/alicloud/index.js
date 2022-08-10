8d14eb3065974ba270806e02ebb750a72cfb843e
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-10 09:24:33 +0000

    user alicoud services

---
+++ miniprogram/alicloud/index.js
@@ -0,0 +1,8 @@
+import MPServerless from '@alicloud/mpserverless-sdk'
+
+export default new MPServerless(wx, {
+    appId: '小程序 AppID',
+    spaceId: '服务空间 SpaceId',
+    clientSecret: '服务空间 Secret',
+    endpoint: '服务空间 API Endpoint'
+});
