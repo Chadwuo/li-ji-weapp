a5fdb1564f758ee6ba499afa1db46e23990757f8
Chadwuo <chadwuo@outlook.com>
2022-08-14 21:49:03 +0800

    gogogo

--- miniprogram/components/friend-edit/index.js
+++ miniprogram/components/friend-edit/index.js
@@ -2,66 +2,65 @@ import pinyin from "wl-pinyin";
 const friendService = require('../../alicloud/services/friend')
 
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
+  /**
+   * 组件的属性列表
+   */
+  properties: {
 
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    visible: false,
+    _id: '',
+    name: '',
+    firstLetter: '',
+    relation: '',
+    remarks: '',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    show(data) {
+      this.setData({
+        visible: true
+      })
     },
-
-    /**
-     * 组件的初始数据
-     */
-    data: {
-        visible: false,
-        _id: '',
-        name: '',
-        firstLetter: '',
-        relation: '',
-        remarks: '',
+    onCancel() {
+      this.setData({
+        visible: false
+      })
     },
-
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        show(data) {
-            console.log(data)
+    async onSave() {
+      if (this.data._id) {
+        const res = await friendService.updataFriend(this.data)
+        if (res.success) {
+          wx.showToast({
+            title: '修改成功',
+          })
+          setTimeout(() => {
             this.setData({
-                visible: true
+              visible: false
             })
-        },
-        onCancel() {
+          }, 500);
+        }
+      } else {
+        const res = await friendService.addFriend(this.data)
+        if (res.success) {
+          wx.showToast({
+            title: '添加成功',
+          })
+          setTimeout(() => {
             this.setData({
-                visible: false
+              visible: false
             })
-        },
-      async  onSave() {
-            if (this.data._id) {
-                const res = await friendService.updataFriend(this.data)
-                if (res.success) {
-                    wx.showToast({
-                        title: '修改成功',
-                    })
-                    setTimeout(() => {
-                        this.setData({
-                            visible: false
-                        })
-                    }, 500);
-                }
-            } else {
-                const res = await friendService.addFriend(this.data)
-                if (res.success) {
-                    wx.showToast({
-                        title: '添加成功',
-                    })
-                    setTimeout(() => {
-                        this.setData({
-                            visible: false
-                        })
-                    }, 500);
-                }
-            }
-        },
-    }
+          }, 500);
+        }
+      }
+    },
+  }
 })
