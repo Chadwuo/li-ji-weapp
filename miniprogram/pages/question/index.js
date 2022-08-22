2f0d29ec44943d69873c48bfe1fa01fb8c3b0c59
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-22 09:09:02 +0000

    add question page

---
+++ miniprogram/pages/question/index.js
@@ -0,0 +1,67 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.loadData(1)
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.setData({
+      issuesList: []
+    })
+    this.loadData(1)
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 2000);
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.loadData(this.data.pageNo + 1)
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
