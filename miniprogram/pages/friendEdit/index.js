80687dfd93d914f6af4b25ba0d02f8745b94f07a
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-08-30 03:04:09 +0000

    页面重构

--- miniprogram/pages/friendEdit/index.js
+++ miniprogram/pages/friendEdit/index.js
@@ -1,18 +1,89 @@
 // pages/friendEdit/index.js
+const friendService = require('../../alicloud/services/friend')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    _id: '',
+    name: '',
+    firstLetter: '',
+    relation: '',
+    remarks: '',
   },
-
+  async onSave() {
+    if (this.data._id) {
+      const res = await friendService.updataFriend(this.data)
+      if (res.success) {
+        wx.showToast({
+          title: '修改成功',
+        })
+        setTimeout(() => {
+          eventChannel.emit('dialogResult', {
+            type: 'update',
+            data: this.data
+          });
+          wx.navigateBack()
+        }, 1000);
+      }
+    } else {
+      const res = await friendService.addFriend(this.data)
+      if (res.success) {
+        wx.showToast({
+          title: '添加成功',
+        })
+        setTimeout(() => {
+          this.data._id = res.data
+          eventChannel.emit('dialogResult', {
+            type: 'insert',
+            data: this.data
+          });
+          wx.navigateBack()
+        }, 1000);
+      }
+    }
+  },
+  async onDelete() {
+    let delData = this.data
+    const eventChannel = this.getOpenerEventChannel()
+    wx.showModal({
+      title: '删除亲友？',
+      content: '该亲友所有来往记录都将被删除，确定删除？',
+      async success(res) {
+        if (res.confirm) {
+          const result = await friendService.deleteFriend(delData)
+          if (result.success) {
+            wx.showToast({
+              title: '删除成功',
+            })
+            setTimeout(() => {
+              eventChannel.emit('dialogResult', {
+                type: 'delete',
+                data: delData
+              });
+              wx.navigateBack()
+            }, 1000);
+          }
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    var id = options.friendId
+    if (id) {
+      const res = await friendService.getFriend({
+        _id: id
+      })
+      if (res.success) {
+        this.setData({
+          ...res.data
+        })
+      }
+    }
   },
 
   /**
--- miniprogram/pages/giftOutEdit/index.js
+++ miniprogram/pages/giftOutEdit/index.js
@@ -1,5 +1,5 @@
-// pages/bookEdit/index.js
-const bookService = require('../../alicloud/services/book')
+// pages/giftEdit/index.js
+const giftOutService = require('../../alicloud/services/giftOut')
 Page({
 
   /**
@@ -7,14 +7,16 @@ Page({
    */
   data: {
     _id: '',
-    date: new Date(),
+    friendId: '',
+    friendName: '',
     title: '',
+    date: '',
+    money: '',
     remarks: '',
   },
   async onSave() {
-    const eventChannel = this.getOpenerEventChannel()
     if (this.data._id) {
-      const res = await bookService.updateBook(this.data)
+      const res = await giftOutService.updataGiftOut(this.data)
       if (res.success) {
         wx.showToast({
           title: '修改成功',
@@ -28,13 +30,12 @@ Page({
         }, 1000);
       }
     } else {
-      const res = await bookService.addBook(this.data)
+      const res = await giftOutService.addGiftOut(this.data)
       if (res.success) {
         wx.showToast({
-          title: '保存成功',
+          title: '添加成功',
         })
         setTimeout(() => {
-          this.data._id = res.data
           eventChannel.emit('dialogResult', {
             type: 'insert',
             data: this.data
@@ -48,11 +49,11 @@ Page({
     let delData = this.data
     const eventChannel = this.getOpenerEventChannel()
     wx.showModal({
-      title: '删除礼簿？',
-      content: '该礼簿所有来往记录都将被删除，确定删除？',
+      title: '删除来往记录？',
+      content: '此操作无法恢复，确定删除？',
       async success(res) {
         if (res.confirm) {
-          const result = await bookService.deleteBook(delData)
+          const result = await giftOutService.deleteGiftOut(delData)
           if (result.success) {
             wx.showToast({
               title: '删除成功',
@@ -80,23 +81,27 @@ Page({
       }
     });
   },
+  showFriendSelect() {
+    wx.navigateTo({
+      url: '/pages/calendar/index',
+      events: {
+        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
+        dialogResult: function (data) {
+          that.bookEditDialog(data)
+        },
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  async onLoad(options) {
-    var id = options.bookId
-    if (id) {
-      const res = await bookService.getBook({
-        _id: id
+  onLoad(options) {
+    const data = options.data
+    if (data) {
+      this.setData({
+        navBarTitle: '编辑送礼',
+        ...data
       })
-      if (res.success) {
-        wx.setNavigationBarTitle({
-          title: '编辑礼簿'
-        })
-        this.setData({
-          ...res.data
-        })
-      }
     }
   },
 
