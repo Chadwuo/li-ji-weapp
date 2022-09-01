da3dbf25a5275bf083222b571b06d4426b4dbfa6
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-09-01 06:39:45 +0000

    fix bug

--- miniprogram/pages/giftOut/index.js
+++ miniprogram/pages/giftOut/index.js
@@ -50,7 +50,7 @@ Page({
   },
   onAddGift() {
     wx.navigateTo({
-      url: '/pages/giftReceive/edit/index',
+      url: '/pages/giftOut/edit/index',
       events: {
         // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
         dialogResult: function (data) {
