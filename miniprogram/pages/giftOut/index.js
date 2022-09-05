d82141a9e244a7ed9a25b921a5af7af801429f1a
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-09-05 02:16:49 +0000

    优化，送礼收礼填写页面逻辑

--- miniprogram/pages/giftOut/index.js
+++ miniprogram/pages/giftOut/index.js
@@ -51,6 +51,25 @@ Page({
         break;
     }
   },
+  onGiftClick(e) {
+    // TODO 需要处理
+    let that = this
+    wx.navigateTo({
+      url: '/pages/giftOut/edit/index',
+      success: function (res) {
+        // 通过 eventChannel 向被打开页面传送数据
+        res.eventChannel.emit('acceptDataFromOpenerPage', {
+          _id: '',
+          friendId: '',
+          friendName: '',
+          title: '',
+          date: {},
+          money: '',
+          remarks: '',
+        })
+      }
+    });
+  },
   onAddGift() {
     let that = this
     wx.navigateTo({
