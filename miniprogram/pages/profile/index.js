bfa3ed50b9c676df2b9b6a722d92dd3e04dd8aab
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-09-08 05:53:05 +0000

    修改；个人中心页面； 新增：个人资料编辑页面

--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -1,88 +1,16 @@
 // pages/profile/index.js
-const giftOutService = require('../../alicloud/services/giftOut')
-const giftReceiveService = require('../../alicloud/services/giftReceive')
-const jinrishici = require('../../utils/jinrishici.js')
-
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    welcome: '',
-    giveTotal: 0.00,
-    receiveTotal: 0.00,
-    MyMenus: [{
-        page: "family",
-        icon: "shop-o",
-        name: "我的家庭"
-      },
-      {
-        page: "export",
-        icon: "peer-pay",
-        name: "数据导出"
-      },
-      {
-        page: "chart",
-        icon: "chart-trending-o",
-        name: "统计分析"
-      },
-      {
-        page: "sponsor",
-        icon: "good-job-o",
-        name: "赞赏"
-      },
-      {
-        page: "question",
-        icon: "question-o",
-        name: "常见问题"
-      },
-      {
-        page: "issue",
-        icon: "smile-comment-o",
-        name: "意见反馈"
-      },
-      {
-        page: "share",
-        icon: "share-o",
-        open_type: 'share',
-        name: "分享"
-      },
-      {
-        page: "about",
-        icon: "info-o",
-        name: "关于"
-      }
-    ]
-  },
-  jumpPage(e) {
-    if (e.currentTarget.dataset.page === 'chart') {
-      wx.showToast({
-        title: '统计分析，马上写完，真的...',
-        icon: 'none',
-      })
-      return
-    }
-    if (e.currentTarget.dataset.page === 'export') {
-      wx.showToast({
-        title: '数据导出，马上写完，真的...',
-        icon: 'none',
-      })
-      return
-    }
-
-    wx.navigateTo({
-      url: `/pages/${e.currentTarget.dataset.page}/index`,
-    });
+    
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    jinrishici.load(result => {
-      this.setData({
-        welcome: result.data.content
-      })
-    })
+
   },
 
   /**
@@ -96,16 +24,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   async onShow() {
-    const res = await giftReceiveService.computedTotalGiftReceive()
-    this.setData({
-      receiveTotal: res.data,
-    });
-    const res1 = await giftOutService.computedTotalGiftOut()
-    this.setData({
-      giveTotal: res1.data,
-    });
-    console.log(res)
-    console.log(res1)
+
   },
 
   /**
@@ -140,10 +59,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    return {
-      title: '可能是东半球最好用的人情记账工具',
-      path: "pages/index/index",
-      imageUrl: '../../images/poster.png'
-    }
+
   }
 })
