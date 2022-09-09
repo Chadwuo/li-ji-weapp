060a40a0c6d46b4408eb1c2146f087e8d48cb695
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2022-09-09 02:36:03 +0000

    移除无效代码

--- miniprogram/pages/question/index.js
+++ miniprogram/pages/question/index.js
@@ -20,9 +20,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    this.loadData(1)
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -42,20 +40,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.setData({
-      issuesList: []
-    })
-    this.loadData(1)
-    setTimeout(() => {
-      wx.stopPullDownRefresh()
-    }, 2000);
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    this.loadData(this.data.pageNo + 1)
+
   },
 
   /**
