c72336bc708d2498346c35046eb16991fe44f204
Chadwuo <chadwuo@outlook.com>
2022-09-14 21:36:47 +0800

    Update index.js

--- miniprogram/pages/family/index.js
+++ miniprogram/pages/family/index.js
@@ -4,95 +4,102 @@ const app = getApp();
 import Notify from '@vant/weapp/notify/notify';
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    _id: '',
-    name: '',
-    familyMembers: [],
-  },
-  async onUpdateName() {
-    const res = await familyService.updateFamily({
-      name: this.data.name
-    })
-    if (res.success) {
-      Notify({ type: 'success', message: '家庭名称已保存' });
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        _id: '',
+        name: '',
+        familyMembers: [],
+    },
+    async onUpdateName() {
+        const res = await familyService.updateFamily({
+            name: this.data.name
+        })
+        if (res.success) {
+            Notify({
+                type: 'success',
+                message: '家庭名称已保存'
+            });
+        }
+    },
+    async onCreate() {
+        const res = await familyService.addFamily({
+            name: `${app.userInfo.nickName}的家庭`
+        })
+        if (res.success) {
+            app.userInfo.familyId = res.data
+            wx.setStorageSync('user', app.userInfo)
+            this.onShow()
+        }
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    async onLoad(options) {
+        if (options && options.familyId) {
+            const res = await familyService.addFamilyMember({
+                familyId: options.familyId,
+                relation: '成员'
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    async onShow() {
+        const res = await familyService.getFamilyInfo()
+        if (res.success) {
+            this.setData({
+                ...res.data
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+        return {
+            title: '和我一起记录家里的人情往来',
+            path: "pages/family/index?familyId=" + this.data._id,
+            imageUrl: '../../images/share.jpg'
+        }
     }
-  },
-  async onCreate() {
-    const res = await familyService.addFamily({
-      name: `${app.userInfo.nickName}的家庭`
-    })
-    if (res.success) {
-      app.userInfo.familyId = res.data
-      wx.setStorageSync('user', app.userInfo)
-      this.onLoad()
-    }
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  async onLoad(options) {
-    const res = await familyService.getFamilyInfo()
-    if (res.success) {
-      this.setData({
-        ...res.data
-      })
-    }
-    console.log(res)
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-    return {
-      title: '和我一起记录家里的人情往来',
-      path: "pages/family/index",
-      imageUrl: '../../images/share.jpg'
-    }
-  }
 })
