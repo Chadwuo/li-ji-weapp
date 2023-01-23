90930c00e5d3729f7b3d9db9effb5fc050a74020
Chadwuo <chadwuo@outlook.com>
2023-01-23 16:46:12 +0800

    🐞 fix: 分享功能优化

--- miniprogram/pages/relationship/index.js
+++ miniprogram/pages/relationship/index.js
@@ -144,5 +144,19 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  /**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+		return {
+      title: '亲戚关系计算器',
+      path: "pages/relationship/index",
+		}
+	},
+	onShareTimeline: function () {
+		return {
+			title: '亲戚关系计算器'
+		}
+	}
 })
