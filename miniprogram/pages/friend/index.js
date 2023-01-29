bf22cd1e0b0c560aa67e20605a49a7345c1ac750
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2023-01-29 06:13:22 +0000

    📃 docs: 优化分享功能

--- miniprogram/pages/friend/index.js
+++ miniprogram/pages/friend/index.js
@@ -107,35 +107,41 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {},
+  onReady() { },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() { },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {},
+  onHide() { },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {},
+  onUnload() { },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {},
+  onPullDownRefresh() { },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {},
+  onReachBottom() { },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {}
+  onShareAppMessage() {
+    return {
+      title: '可能是东半球最好用的人情记账工具',
+      path: "pages/start/index",
+      imageUrl: '/static/img/share.jpg'
+    }
+  }
 })
