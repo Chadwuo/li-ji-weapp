fff4bcff086eb0c77ca3539683818acbd225aefe
Chad Wuo <chadwuo@outlook.com>
2023-09-08 23:31:23 +0800

    🎈 perf: 全新日历组件

--- miniprogram/pages/book/edit/index.js
+++ miniprogram/pages/book/edit/index.js
@@ -1,16 +1,43 @@
 // pages/book/edit/index.js
-const bookService = require('../../../alicloud/services/book');
+const bookService = require("../../../alicloud/services/book");
 const app = getApp();
+
+import solarLunar from "../../../components/calendar/plugins/solarLunar/index";
+import holidays from "../../../components/calendar/plugins/holidays/index";
+import plugin from "../../../components/calendar/plugins/index";
+plugin.use(solarLunar).use(holidays);
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    _id: '',
+    _id: "",
     date: {},
-    title: '',
-    remarks: '',
-    tips: ''
+    title: "",
+    remarks: "",
+    tips: "",
+    showCalendar: false,
+    calendarConfig: {
+      //multi: true, // 是否开启多选,
+      //weekMode: true, // 周视图模式
+      theme: "default", // 日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题色在参考 /theme 文件夹
+      showLunar: true, // 是否显示农历，此配置会导致 setTodoLabels 中 showLabelAlways 配置失效
+      //inverse: true, // 单选模式下是否支持取消选中,
+      markToday: "今", // 当天日期展示不使用默认数字，用特殊文字标记
+      //hideHeader: true, // 隐藏日历头部操作栏
+      //takeoverTap: true, // 是否完全接管日期点击事件（日期不会选中)
+      emphasisWeek: true, // 是否高亮显示周末日期
+      //chooseAreaMode: true, // 开启日期范围选择模式，该模式下只可选择时间段
+      showHolidays: true, // 显示法定节假日班/休情况，需引入holidays插件
+      showFestival: true, // 显示节日信息（如教师节等），需引入holidays插件
+      highlightToday: true, // 是否高亮显示当天，区别于选中样式（初始化时当天高亮并不代表已选中当天）
+      //defaultDate: '2018-3-6', // 默认选中指定某天，如需选中需配置 autoChoosedWhenJump: true
+      //preventSwipe: true, // 是否禁用日历滑动切换月份
+      //firstDayOfWeek: 'Mon', // 每周第一天为周一还是周日，默认按周日开始
+      onlyShowCurrentMonth: true, // 日历面板是否只显示本月日期
+      autoChoosedWhenJump: true, // 设置默认日期及跳转到指定日期后是否需要自动选中
+    },
   },
   async onSave() {
     const eventChannel = this.getOpenerEventChannel();
@@ -18,22 +45,26 @@ Page({
       const res = await bookService.updateBook(this.data);
       if (res.success) {
         wx.showToast({
-          title: '修改成功',
+          title: "修改成功",
         });
-        eventChannel.emit('refresh');
+        eventChannel.emit("refresh");
         setTimeout(() => {
-          wx.navigateBack({delta:2});
+          wx.navigateBack({
+            delta: 2,
+          });
         }, 1000);
       }
     } else {
       const res = await bookService.addBook(this.data);
       if (res.success) {
         wx.showToast({
-          title: '保存成功',
+          title: "保存成功",
         });
-        eventChannel.emit('refresh');
+        eventChannel.emit("refresh");
         setTimeout(() => {
-          wx.navigateBack({delta:2});
+          wx.navigateBack({
+            delta: 2,
+          });
         }, 1000);
       }
     }
@@ -42,37 +73,64 @@ Page({
     let delData = this.data;
     const eventChannel = this.getOpenerEventChannel();
     wx.showModal({
-      title: '删除礼簿？',
-      content: '该礼簿所有来往记录都将被删除，确定删除？',
+      title: "删除礼簿？",
+      content: "该礼簿所有来往记录都将被删除，确定删除？",
       async success(res) {
         if (res.confirm) {
           const result = await bookService.deleteBook(delData);
           if (result.success) {
             wx.showToast({
-              title: '删除成功',
+              title: "删除成功",
             });
             app.refreshTotal();
-            eventChannel.emit('refresh');
+            eventChannel.emit("refresh");
             setTimeout(() => {
-              wx.navigateBack();
+              wx.navigateBack({
+                delta: 2,
+              });
             }, 1000);
           }
         }
       },
     });
   },
-  showCalendar() {
-    let that = this;
-    wx.navigateTo({
-      url: `/pages/calendar/index?date=${this.data.date.value}`,
-      events: {
-        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-        handleCalendarDateChange: function (data) {
-          that.setData({
-            date: data,
-          });
-        },
-      },
+  onOpenCalendar() {
+    this.setData({
+      showCalendar: true,
+    });
+    const calendar = this.selectComponent("#calendar").calendar;
+    const { year, month, day } = this.data.date || {};
+    if (year && month && day) {
+      calendar.jump({
+        year,
+        month,
+        date: day,
+      });
+    }
+  },
+  onClosePopup() {
+    this.setData({
+      showCalendar: false,
+    });
+  },
+  /**
+   * 选择日期后执行的事件
+   */
+  afterTapDate(e) {
+    let { year, month, date, lunar } = e.detail;
+    let selectedDate = {
+      year,
+      month,
+      day: date,
+      value: `${year}-${month}-${date}`,
+      lunar_day: lunar.IDayCn,
+      lunar_month: lunar.IMonthCn,
+      lunar_year: `${lunar.gzYear}${lunar.Animal}年`,
+      lunar_term: lunar.Term,
+    };
+    console.log("afterTapDate", selectedDate); // => { year: 2019, month: 12, date: 3, ...}
+    this.setData({
+      date: selectedDate,
     });
   },
   /**
@@ -80,13 +138,12 @@ Page({
    */
   async onLoad(options) {
     const eventChannel = this.getOpenerEventChannel();
-    eventChannel.on('acceptDataFromOpenerPage', (data) => {
+    eventChannel.on("acceptDataFromOpenerPage", (data) => {
       this.setData({
         ...data,
       });
     });
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -97,13 +154,13 @@ Page({
    */
   onShow() {
     const arr = [
-      '一场宴席活动中，用来登记所有来客贺礼的名册，称为礼簿。',
-      '长按礼簿，进入编辑页',
+      "一场宴席活动中，用来登记所有来客贺礼的名册，称为礼簿。",
+      "长按礼簿，进入编辑页",
     ];
     const index = Math.floor(Math.random() * arr.length);
     this.setData({
-      tips: arr[index]
-    })
+      tips: arr[index],
+    });
   },
 
   /**
--- miniprogram/pages/giftOut/edit/index.js
+++ miniprogram/pages/giftOut/edit/index.js
@@ -1,61 +1,87 @@
 // pages/giftOut/edit/index.js
-const giftOutService = require('../../../alicloud/services/giftOut');
+const giftOutService = require("../../../alicloud/services/giftOut");
 const app = getApp();
+
+import solarLunar from "../../../components/calendar/plugins/solarLunar/index";
+import holidays from "../../../components/calendar/plugins/holidays/index";
+import plugin from "../../../components/calendar/plugins/index";
+plugin.use(solarLunar).use(holidays);
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    _id: '',
-    friendId: '',
-    friendName: '',
-    title: '',
-    icon: '',
+    _id: "",
+    friendId: "",
+    friendName: "",
+    title: "",
+    icon: "",
     date: {},
-    money: '',
-    remarks: '',
+    money: "",
+    remarks: "",
     columns: [
       {
-        name: '结婚',
-        icon: 'iconfont icon-jiehun',
+        name: "结婚",
+        icon: "iconfont icon-jiehun",
       },
       {
-        name: '宝宝',
-        icon: 'iconfont icon-qinzi',
+        name: "宝宝",
+        icon: "iconfont icon-qinzi",
       },
       {
-        name: '周岁',
-        icon: 'iconfont icon-zhousui',
+        name: "周岁",
+        icon: "iconfont icon-zhousui",
       },
       {
-        name: '乔迁',
-        icon: 'iconfont icon-qiaoqian',
+        name: "乔迁",
+        icon: "iconfont icon-qiaoqian",
       },
       {
-        name: '生日',
-        icon: 'iconfont icon-shengri',
+        name: "生日",
+        icon: "iconfont icon-shengri",
       },
       {
-        name: '升学',
-        icon: 'iconfont icon-jiaoyu',
+        name: "升学",
+        icon: "iconfont icon-jiaoyu",
       },
       {
-        name: '大寿',
-        icon: 'iconfont icon-laoren',
+        name: "大寿",
+        icon: "iconfont icon-laoren",
       },
       {
-        name: '探望',
-        icon: 'iconfont icon-yiyuan',
+        name: "探望",
+        icon: "iconfont icon-yiyuan",
       },
       {
-        name: '白事',
-        icon: 'iconfont icon-sangzang',
+        name: "白事",
+        icon: "iconfont icon-sangzang",
       },
       {
-        name: '其他',
-        icon: 'cicon-moneybag-o',
+        name: "其他",
+        icon: "cicon-moneybag-o",
       },
     ],
+    calendarConfig: {
+      //multi: true, // 是否开启多选,
+      //weekMode: true, // 周视图模式
+      theme: "default", // 日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题色在参考 /theme 文件夹
+      showLunar: true, // 是否显示农历，此配置会导致 setTodoLabels 中 showLabelAlways 配置失效
+      //inverse: true, // 单选模式下是否支持取消选中,
+      markToday: "今", // 当天日期展示不使用默认数字，用特殊文字标记
+      //hideHeader: true, // 隐藏日历头部操作栏
+      //takeoverTap: true, // 是否完全接管日期点击事件（日期不会选中)
+      emphasisWeek: true, // 是否高亮显示周末日期
+      //chooseAreaMode: true, // 开启日期范围选择模式，该模式下只可选择时间段
+      showHolidays: true, // 显示法定节假日班/休情况，需引入holidays插件
+      showFestival: true, // 显示节日信息（如教师节等），需引入holidays插件
+      highlightToday: true, // 是否高亮显示当天，区别于选中样式（初始化时当天高亮并不代表已选中当天）
+      //defaultDate: '2018-3-6', // 默认选中指定某天，如需选中需配置 autoChoosedWhenJump: true
+      //preventSwipe: true, // 是否禁用日历滑动切换月份
+      //firstDayOfWeek: 'Mon', // 每周第一天为周一还是周日，默认按周日开始
+      onlyShowCurrentMonth: true, // 日历面板是否只显示本月日期
+      autoChoosedWhenJump: true, // 设置默认日期及跳转到指定日期后是否需要自动选中
+    },
   },
   iconChange(e) {
     let { selected } = e.currentTarget.dataset;
@@ -71,9 +97,9 @@ Page({
       const res = await giftOutService.updateGiftOut(this.data);
       if (res.success) {
         wx.showToast({
-          title: '修改成功',
+          title: "修改成功",
         });
-        eventChannel.emit('refresh');
+        eventChannel.emit("refresh");
         app.refreshTotal();
         setTimeout(() => {
           wx.navigateBack();
@@ -83,9 +109,9 @@ Page({
       const res = await giftOutService.addGiftOut(this.data);
       if (res.success) {
         wx.showToast({
-          title: '添加成功',
+          title: "添加成功",
         });
-        eventChannel.emit('refresh');
+        eventChannel.emit("refresh");
         app.refreshTotal();
         setTimeout(() => {
           wx.navigateBack();
@@ -97,16 +123,16 @@ Page({
     let delData = this.data;
     const eventChannel = this.getOpenerEventChannel();
     wx.showModal({
-      title: '删除来往记录？',
-      content: '此操作无法恢复，确定删除？',
+      title: "删除来往记录？",
+      content: "此操作无法恢复，确定删除？",
       async success(res) {
         if (res.confirm) {
           const result = await giftOutService.deleteGiftOut(delData);
           if (result.success) {
             wx.showToast({
-              title: '删除成功',
+              title: "删除成功",
             });
-            eventChannel.emit('refresh');
+            eventChannel.emit("refresh");
             app.refreshTotal();
             setTimeout(() => {
               wx.navigateBack();
@@ -116,28 +142,52 @@ Page({
       },
     });
   },
-  showCalendar() {
-    let that = this;
-    wx.navigateTo({
-      url: `/pages/calendar/index?date=${this.data.date.value}`,
-      events: {
-        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-        handleCalendarDateChange: function (data) {
-          that.setData({
-            date: data,
-          });
-        },
-      },
+  onOpenCalendar() {
+    this.setData({
+      showCalendar: true,
+    });
+    const calendar = this.selectComponent("#calendar").calendar;
+    const { year, month, day } = this.data.date || {};
+    if (year && month && day) {
+      calendar.jump({
+        year,
+        month,
+        date: day,
+      });
+    }
+  },
+  onClosePopup() {
+    this.setData({
+      showCalendar: false,
+    });
+  },
+  /**
+   * 选择日期后执行的事件
+   */
+  afterTapDate(e) {
+    let { year, month, date, lunar } = e.detail;
+    let selectedDate = {
+      year,
+      month,
+      day: date,
+      value: `${year}-${month}-${date}`,
+      lunar_day: lunar.IDayCn,
+      lunar_month: lunar.IMonthCn,
+      lunar_year: `${lunar.gzYear}${lunar.Animal}年`,
+      lunar_term: lunar.Term,
+    };
+    console.log("afterTapDate", selectedDate); // => { year: 2019, month: 12, date: 3, ...}
+    this.setData({
+      date: selectedDate,
     });
   },
   showFriendSelect() {
-    let that = this;
     wx.navigateTo({
-      url: '/pages/friend/select/index',
+      url: "/pages/friend/select/index",
       events: {
         // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-        dialogResult: function (data) {
-          that.setData({
+        dialogResult: (data) => {
+          this.setData({
             friendId: data._id,
             friendName: data.name,
           });
@@ -150,12 +200,12 @@ Page({
    */
   onLoad(options) {
     const eventChannel = this.getOpenerEventChannel();
-    eventChannel.on('acceptDataFromOpenerPage', (data) => {
+    eventChannel.on("acceptDataFromOpenerPage", (data) => {
       this.setData({
         ...data,
       });
       wx.setNavigationBarTitle({
-        title: '编辑记录',
+        title: "编辑记录",
       });
     });
   },
