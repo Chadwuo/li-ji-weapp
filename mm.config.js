3aa5a752aaa0e55465cb410a4badf054e7a5b5a6
wll8 <xwdaysme@gmail.com>
2023-11-25 20:26:56 +0800

    feat: 移除 mpserverless

--- mm.config.js
+++ mm.config.js
@@ -26,10 +26,13 @@ module.exports = util => {
 
     // 自己编写的接口
     api: {
-      // 可以作为守卫使用
+      "use /todo/"(req, res) {
+        res.status(404).json({})
+      },
       async 'use /books' (req, res, next) {
         if(req.query.page) {
           res.mm.resHandleJsonApi = (arg) => {
+            // 统计单个账本的人数和金额
             arg.data.results.map((i) => {
               // 多少人
               i.giftCount = 10
@@ -42,27 +45,50 @@ module.exports = util => {
         }
         next()
       },
+      // 收入
+      async 'get /computedTotalGiftReceive' (req, res, next) {
+        const db = global.config._db 
+        res.json(123)
+      },
+      // 送出
+      async 'get /computedTotalGiftOut' (req, res, next) {
+        const db = global.config._db 
+        res.json(1353)
+      },
     },
     
     // 使用 mockjs 生成数据以及 Restful API
     dbCover: true,
     db: util.libObj.mockjs.mock({
-      'books|1-3': [
+      'books': [
+        // {
+        //   "_id": `1`,
+        //   "userId": "1",
+        //   "date": {
+        //     "year": 2023,
+        //     "month": 11,
+        //     "day": 16,
+        //     "value": "2023-11-16",
+        //     "lunar_day": "初四",
+        //     "lunar_month": "十月",
+        //     "lunar_year": "癸卯兔年",
+        //     "lunar_term": null
+        //   },
+        //   "title": "@ctitle",
+        //   "remarks": "@ctitle",
+        // },
+      ],
+      'family_members': [],
+      'users': [
         {
-          "_id|+1": 1,
-          "userId": "@uuid",
-          "date": {
-            "year": 2023,
-            "month": 11,
-            "day": 16,
-            "value": "2023-11-16",
-            "lunar_day": "初四",
-            "lunar_month": "十月",
-            "lunar_year": "癸卯兔年",
-            "lunar_term": null
-          },
-          "title": "@ctitle",
-          "remarks": "@ctitle",
+          "id": "todoid",
+          "nickName": "微信用户",
+          "avatarUrl": "https://mp-ca131de3-6a07-4205-8ee0-970a5c092c09.cdn.bspapp.com/cloudstorage/0114d087-7245-4fe5-9de4-ea88fdc14066.jpg",
+          "isVip": false,
+          "oAuthUserId": "todoid",
+          "createdAt": "2023-11-19T17:47:26+08:00",
+          "lastSeenAt": "2023-11-25T17:14:35+08:00",
+          "skipAD": true
         },
       ],
     }),
