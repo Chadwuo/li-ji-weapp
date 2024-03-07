a671987c32f0fccd6a137cb7e36f21536502bd49
Chad Wuo <chadwuo@outlook.com>
2024-03-07 21:44:06 +0800

    ✨ feat: 3-7

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div mx-auto mt-5 text-center text-xs text-gray opacity-50>
+  <div mx-auto pt-5 pb-2 text-center text-xs text-gray opacity-50>
     <div>每一份人情都值得礼记</div>
     <div>Copyright © Chadwuo Design. All Rights Reserved</div>
   </div>
--- src/pages/book/edit.vue
+++ src/pages/book/edit.vue
@@ -1,17 +1,18 @@
 <template>
     <div>
-        <uv-empty mode="car" icon="https://cdn.uviewui.com/uview/empty/car.png"></uv-empty>
-        <uv-form labelPosition="left">
-            <uv-form-item label="礼簿名称" >
-                <uv-input v-model="book.title" >
+        <uv-empty icon="https://cdn.uviewui.com/uview/empty/car.png"></uv-empty>
+        <uv-form labelPosition="left" labelWidth="90">
+            <uv-form-item label="日期" borderBottom>
+                <uv-input v-model="book.title" border="none">
                 </uv-input>
             </uv-form-item>
-            <uv-form-item label="日期" borderBottom @click="showSexSelect">
-                <uv-input v-model="book.sex" disabled disabledColor="#ffffff" placeholder="请选择性别" border="none">
+            <uv-form-item label="礼簿名称" borderBottom>
+                <uv-input v-model="book.title" border="none">
                 </uv-input>
-                <template v-slot:right>
-                    <uv-icon name="arrow-right"></uv-icon>
-                </template>
+            </uv-form-item>
+            <uv-form-item label="备注" borderBottom>
+                <uv-input v-model="book.title" border="none">
+                </uv-input>
             </uv-form-item>
             <uv-button type="primary" text="提交" customStyle="margin-top: 10px" @click="submit"></uv-button>
             <uv-button type="error" text="重置" customStyle="margin-top: 10px" @click="reset"></uv-button>
@@ -40,3 +41,7 @@ const book = ref({});
 </script>
 
 <style lang="scss" scoped></style>
+
+<route lang="json">{
+    "layout": "home"
+}</route>
