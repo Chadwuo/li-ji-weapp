951dee9c5d2ef038df9ccfce5352e159d9692234
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2024-03-10 12:57:36 +0800

    ✨ feat: 3-10

--- src/pages/book/edit.vue
+++ src/pages/book/edit.vue
@@ -4,7 +4,7 @@
             <uv-form labelPosition="left" labelWidth="120">
                 <uv-form-item label="日期" borderBottom leftIcon="calendar" leftIconStyle="color:#F87171"
                     @click="calendarRef.open()">
-                    <uv-input v-model="dataSource.title" disabled disabledColor="#ffffff" border="none"
+                    <uv-input v-model="dataSource.date.value" disabled disabledColor="#ffffff" border="none"
                         placeholder="请选择日期">
                     </uv-input>
                     <template v-slot:right>
@@ -21,15 +21,16 @@
                 </uv-form-item>
             </uv-form>
         </div>
-        <div class="card mt-4">
+        <view class="card mt-4">
             <ad unit-id="adunit-64aefbe92c2dc7bf"></ad>
-            <div class="text-xs text-gray mt-1">广告可以在设置中关闭</div>
-        </div>
+            <view class="text-xs text-gray mt-1">广告可以在设置中关闭</view>
+        </view>
     </div>
 
     <div class="fixed bottom-12 w-full">
         <div class="flex space-x-4 mx-4">
-            <uv-button text="删除" shape="circle" @click="submit" :loading="loading" loadingMode="circle"></uv-button>
+            <uv-button text="删除" shape="circle" @click="submit" :loading="loading" loadingMode="circle"
+                v-if="dataSource._id"></uv-button>
             <div class="w-full">
                 <uv-button type="primary" text="保存" shape="circle" @click="submit" :loading="loading"
                     loadingMode="circle"></uv-button>
@@ -37,41 +38,67 @@
         </div>
     </div>
 
-    <uv-calendars ref="calendarRef" @confirm="confirm" lunar />
+    <uv-calendars ref="calendarRef" @confirm="calendarConfirm" lunar color="#F87171" confirmColor="#F87171"
+        :date="dataSource.date.value" />
 </template>
 
 <script setup>
 import { onLoad } from "@dcloudio/uni-app";
+import { getBookInfo, addBook, updateBook, deleteBook } from '~/alicloud/services/book'
 const calendarRef = ref(null);
 
 // onLoad 接受 A 页面传递的参数
 onLoad((option) => {
     console.log("参数", option);
-    if (option.id) {
-        uni.setNavigationBarTitle({
-            title: '新的标题'
-        });
-    } else {
+    if (!option.id) {
         uni.setNavigationBarTitle({
             title: '新增礼簿'
         });
+    } else {
+        uni.setNavigationBarTitle({
+            title: '礼簿详情'
+        });
+        getBookInfo({ _id: option.id }).then(res => {
+            console.log('object :>> ', res);
+            dataSource.value = res.result;
+        });
     }
-
 });
 
-const dataSource = ref({});
-const loading = ref(true);
+const dataSource = ref({
+    date: {},
+});
+const loading = ref(false);
 
 const submit = () => {
     calendarRef.value.open();
 }
 
-const confirm = (date) => {
-    console.log(date);
+const calendarConfirm = (e) => {
+    let {
+        year,
+        month,
+        date,
+        lunar
+    } = e;
+    console.log(e);
+
+    let selectedDate = {
+        year,
+        month: Number(month),
+        day: date,
+        value: `${year}-${Number(month)}-${date}`,
+        lunar_day: lunar.IDayCn,
+        lunar_month: lunar.IMonthCn,
+        lunar_year: `${lunar.gzYear}${lunar.Animal}年`,
+        lunar_term: lunar.Term || '',
+    };
+    dataSource.value.date = selectedDate;
     calendarRef.value.close();
 }
 
-const book = ref({});
+
+
 </script>
 
 <style lang="scss" scoped></style>
