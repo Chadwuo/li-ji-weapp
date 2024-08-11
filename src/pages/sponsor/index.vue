281863dcbb7555150959b39137a2b96c53a440f4
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2024-08-11 11:48:43 +0800

    🌈 style: 格式化代码

--- src/pages/sponsor/index.vue
+++ src/pages/sponsor/index.vue
@@ -1,19 +1,15 @@
 <script setup>
-
 </script>
 
 <template>
   <div class="mt-5">
     <div class="indent-8 text-sm leading-relaxed">
-      礼记 是一个开源项目，使用完全免费，维护这样一个项目需要很大的精力和服务器费用。
+      "礼记"作为一个开放共享的项目，一直以来都致力于为用户提供高质量的服务与体验。由于项目的持续运行需要较大的资源投入，我们不得不在部分页面引入了少量广告以支持日常运营。
     </div>
-    <div class="indent-8 text-sm leading-relaxed">
-      为了维持礼记的健康运营，部分页面中增加了广告，敬请理解，当然，如果你对广告容忍度很低，可以在<text
-        class="text-red"
-        @click="router.push('/pages/settings/index')"
-      >
+    <div class="mt-2 indent-8 text-sm leading-relaxed">
+      我们理解即使极少的广告也可能会影响到您的使用体验，为此，我们在<text class="text-red" @click="router.push('/pages/settings/index')">
         设置
-      </text>中关闭广告！
+      </text>中提供了关闭广告的功能。如果您希望支持“礼记”，让这个社区继续成长并为更多人服务，请考虑保持广告开启状态。
     </div>
   </div>
   <Advertisement class="mt-auto" adtyle="video" />
