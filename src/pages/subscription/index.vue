33f301473784c4f1c6da5ed8eb101897a7e72470
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2025-02-12 16:59:16 +0800

    🎈 perf: 锁定sass版本

--- src/pages/subscription/index.vue
+++ src/pages/subscription/index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
+// import { storeToRefs } from 'pinia'
 
 const loading = ref(false)
-const { userInfo } = storeToRefs(useAuthStore())
+// const { userInfo } = storeToRefs(useAuthStore())
 const pay = async () => {
   loading.value = true
   const res = await apiWechatPayCreatePayPost({
@@ -12,38 +12,48 @@ const pay = async () => {
     const payData = res.data.singInfo
     wx.requestPayment({
       ...payData,
-      success(res) {
-        console.log('支付成功', res)
-      },
-      fail(res) {
-        console.log('支付失败', res)
-      },
+      // success(res) {
+      //   console.log('支付成功', res)
+      // },
+      // fail(res) {
+      //   console.log('支付失败', res)
+      // },
       complete() {
         loading.value = false
       },
     })
   }
 }
-
 </script>
 
 <template>
-  <div class="mx-3">
-    
-    <wd-button block :loading="loading" loading-color="#F87171" @click="pay">
-      立即购买
-    </wd-button>
-
-    <div>{{ userInfo?.id }}</div>
-    <div>{{ userInfo?.nickName }}</div>
-    <div>{{ userInfo?.isVip }}</div>
+  <div class="mx-3 h-full flex flex-col items-center">
+    <div class="bg-[url('https://poemcode.cn/liji-oss/assets/subscription/countdown_streamer.png')] bg-contain bg-no-repeat text-center">
+      <div class="mt-6 text-6xl">
+        🎉
+      </div>
+      <div class="mt-4 text-2xl">
+        永久会员限时优化活动
+      </div>
+      <div class="mt-2 text-sm text-gray">
+        为效率和人情充值，让你的时间管理更高效
+      </div>
+    </div>
+    <img src="https://poemcode.cn/liji-oss/assets/subscription/price.png" class="mt-6 w-full">
+    <div class="mt-auto w-full">
+      <wd-button block :loading="loading" loading-color="#F87171" @click="pay">
+        立即购买
+      </wd-button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped></style>
 
-<route lang="json">{
+<route lang="json">
+{
   "style": {
     "navigationBarTitleText": ""
   }
-}</route>
+}
+</route>
