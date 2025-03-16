3da1eeceac3c5ac4479dd21979e9be73132c2ae2
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2025-03-16 23:18:22 +0800

    ✨ feat: 线上环境切换

--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -2,10 +2,22 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
   const searchHistory = ref<Array<string>>([])
+  let baseApiUrl = import.meta.env.VITE_SERVICE_API_URL
+  let version = '4.0.0'
+
+  // #ifdef MP-WEIXIN
+  const { miniProgram } = uni.getAccountInfoSync()
+  version = miniProgram.version
+  baseApiUrl += miniProgram.envVersion === 'release' ? '-release' : '-dev'
+  // #endif
 
   return {
     searchHistory,
+    baseApiUrl,
+    version,
   }
 }, {
-  persist: true,
+  persist: {
+    pick: ['searchHistory'],
+  },
 })
