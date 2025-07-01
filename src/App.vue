7ce45b71b4cadc1c2b35d7fc1de9ebcc9e1d0d51
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2025-07-01 17:30:43 +0800

    refactor(app): 重构 App.vue 中的 onLaunch 方法
    - 移除了对 isLogin 的判断和相关导航逻辑
    - 简化了异常处理
    - 优化了加载用户信息的流程

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,11 @@ onLaunch(async () => {
   uni.showLoading({
     title: '加载中...',
   })
-
   try {
     const authStore = useAuthStore()
-    if (!authStore.isLogin) {
-      uni.navigateTo({
-        url: '/pages/welcome/index',
-      })
-    }
-    else {
-      await authStore.setupApp()
-    }
-  }
-  catch (error) {
-    console.error(error)
-    uni.redirectTo({
-      url: `/pages/exception/500?error=${JSON.stringify(error)}`,
-    })
+    authStore.userInfo = await apiUserInfoGet()
+    authStore.userFamilys = await apiUserFamilyListGet()
+    authStore.friendTags = await apiFriendTagListGet()
   }
   finally {
     uni.hideLoading()
